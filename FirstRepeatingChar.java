cc3471ed4f3f6473a4ed15eb7baefdda8d7138f0
Mithun Kumar S R <mithunkumar.sr@uber.com>
2019-10-13 12:40:04 +0530

    First repeating char

---
+++ FirstRepeatingChar.java
@@ -0,0 +1,30 @@
+
+/******************************************************************************
+CS Tutorial for Children
+Author: SRMK
+
+
+*******************************************************************************/
+import java.util.*;
+
+public class FirstRepeatingChar {
+
+    public static char findFirstRepeating(char str[]) {
+        HashSet<Character> hs = new HashSet<>();
+
+        for (int i = 0; i <= str.length; i++) {
+            char c = str[i];
+            if (hs.contains(c)) {
+                return c;
+            } else
+                hs.add(c);
+        }
+        return '\0';
+    }
+
+    public static void main(String[] args) {
+        String str = "idodldkd";
+        char[] arr = str.toCharArray();
+        System.out.println(findFirstRepeating(arr));
+    }
+}
